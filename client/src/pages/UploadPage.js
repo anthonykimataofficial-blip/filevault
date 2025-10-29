4dfabf67a8630e75d30e6922cd2458e74d4c20fa
Anthony Kimata <anthonykimataofficial@gmail.com>
2025-10-29 12:15:58 +0300

    Fix: Cloudinary signature mismatch

--- client/src/pages/UploadPage.js
+++ client/src/pages/UploadPage.js
@@ -45,10 +45,11 @@ function UploadPage() {
       // 2️⃣ Upload directly to Cloudinary
       const formData = new FormData();
       formData.append('file', file);
-      formData.append('api_key', api_key);
-      formData.append('timestamp', timestamp);
-      formData.append('signature', signature);
-      formData.append('folder', 'filevault_uploads');
+formData.append('api_key', apiKey);
+formData.append('timestamp', timestamp);
+formData.append('signature', signature);
+formData.append('folder', 'filevault_uploads');
+
 
       const uploadRes = await fetch(
         `https://api.cloudinary.com/v1_1/${cloud_name}/auto/upload`,
